4f7968392aa84e062769c81cfaf240f9e328c8d4
Jaytee <Padilla.Jaytee@gmail.com>
2020-11-03 15:44:57 -0800

    Created seed data

--- index.js
+++ index.js
@@ -1,8 +1,47 @@
 const server = require('./server');
 const { sequelize, models } = require('./src/models');
 
+const eraseDatabaseOnSync = true;
+
 sequelize.sync({ force: true }).then(() => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages();
+  }
+
   server.listen(process.env.PORT, () =>
     console.log('\nListening on port 3000\n')
   );
 });
+
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: 'Jaytee',
+      messages: [
+        {
+          text: 'Message 1 for Jaytee'
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+
+  await models.User.create(
+    {
+      username: 'Baiken',
+      messages: [
+        {
+          text: 'Message 1 for Baiken',
+        },
+        {
+          text: 'Message 2 for Baiken',
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+};
