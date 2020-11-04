ce885e024bf053faeb2c84d57f3f4622c1e66f4a
Jaytee <Padilla.Jaytee@gmail.com>
2020-11-04 14:31:57 -0800

    Added routes

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+require('dotenv').config(); // allows data in .env file to be accessed from anywhere via 'process.env'
+
 const express = require('express');
 const cors = require('cors');
-
-require('dotenv').config(); // allows data in .env file to be accessed from anywhere via 'process.env'
+const { models } = require('./src/models');
 
 const server = express();
 
 server.use(cors());
+
+server.use(async (req, res, next) => {
+  req.context = {
+    models,
+    me: await models.User.findByLogin('Jaytee'),
+  };
+
+  next();
+});
 
 server.get('/', (req, res) => {
   res.send('<h2>API is up and running</h2>');
---
+++ src/routes/session.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+  const user = await req.context.models.User.findByPk(
+    req.context.me.id,
+  );
+
+  return res.send(user);
+});
+
+module.exports = router;
